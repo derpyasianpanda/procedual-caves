9dea6beeccd1cfb71529f409354e4fefb79536a5
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-03-03 13:51:46 -0800

    Added map regeneration

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ import sys
 class Map:
     def __init__(self, seed=None):
         self.map_grid = numpy.empty((grid_width, grid_height), float)
-        random.seed(seed)
-        self.seed = int(random.random() * 1000)
-        self.generate_map()
+        self.seed = seed
+        self.base_seed = int(random.random() * 1000)
+        self.regenerate()
 
     def generate_map(self):
         for x in range(grid_width):
             for y in range(grid_height):
-                self.map_grid[x, y] = noise.pnoise2(x / grid_width, y / grid_height,
-                                                    persistence=5, octaves=3, base=self.seed)
+                self.map_grid[x, y] = noise.snoise2(x / grid_width, y / grid_height,
+                                                    persistence=1, octaves=2, base=self.base_seed)
         self.smooth_map()
         self.connect_rooms()
 
@@ -33,9 +33,15 @@ class Map:
                 pygame.draw.rect(screen, [color, color, color],
                                  (tile_width * x, tile_height * y, tile_width, tile_height))
 
+    def regenerate(self):
+        random.seed(self.seed)
+        self.base_seed = int(random.random() * 1000)
+        self.generate_map()
+        self.display()
 
-grid_width = 300
-grid_height = 300
+
+grid_width = 200
+grid_height = 200
 display_width = 600
 display_height = 600
 tile_width = display_width // grid_width
@@ -45,11 +51,13 @@ pygame.init()
 screen = pygame.display.set_mode((display_width, display_height))
 screen.fill([255, 255, 255])
 main = Map()
-main.display()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_F5:
+                main.regenerate()
     pygame.display.flip()
