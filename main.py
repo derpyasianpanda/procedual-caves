387edb93fffa7e28134506d60001f2026340095c
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-03-04 15:19:33 -0800

    Fixed randomness

--- main.py
+++ main.py
@@ -9,19 +9,17 @@ import datetime
 
 class Map:
     def __init__(self, seed=None):
-        self.map_grid = numpy.empty((grid_width, grid_height), float)
+        self.initial_grid = numpy.empty((grid_width, grid_height), float)
+        self.second_grid = None  # Smoothed grid
+        self.final_grid = None  # Connected grid
         self.seed = seed if seed else hash(str(datetime.datetime.now()))
         self.regenerate()
 
-    def generate_map(self):
-        print(self.seed)
+    def generate_initial_grid(self):
         random.seed(self.seed)
-        temp = random.choices([0, 1], k=grid_width * grid_height)
         for y in range(grid_height):
             for x in range(grid_width):
-                self.map_grid[x, y] = temp[10 * y + x]
-        self.smooth_map()
-        self.connect_rooms()
+                self.initial_grid[x, y] = random.randint(0, 1)
 
     def smooth_map(self, factor=1):
         pass
@@ -32,16 +30,14 @@ class Map:
     def display(self):
         for x in range(grid_width):
             for y in range(grid_height):
-                color = self.map_grid[x, y] * 255
+                color = self.initial_grid[x, y] * 255
                 pygame.draw.rect(screen, [color, color, color],
                                  (tile_width * x, tile_height * y, tile_width, tile_height))
                 # pygame.display.update((tile_width * x, tile_height * y, tile_width, tile_height))
 
     def regenerate(self, seed=None):
-        if self.seed:
-            random.seed(self.seed)
         self.seed = seed if seed else hash(str(datetime.datetime.now()))
-        self.generate_map()
+        self.generate_initial_grid()
         self.display()
 
 
