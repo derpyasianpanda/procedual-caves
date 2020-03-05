476ff0ac6ead11c00813fecc0546ff8b86e5414e
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-03-04 16:38:49 -0800

    Changed Random Generation

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ import datetime
 
 class Map:
     def __init__(self, seed=None):
-        self.initial_grid = numpy.empty((grid_width, grid_height), float)
-        self.second_grid = None  # Smoothed grid
-        self.final_grid = None  # Connected grid
+        self.grid = [numpy.empty((grid_width, grid_height), float)]
         self.seed = seed if seed else hash(str(datetime.datetime.now()))
         self.regenerate()
 
@@ -19,26 +17,62 @@ class Map:
         random.seed(self.seed)
         for y in range(grid_height):
             for x in range(grid_width):
-                self.initial_grid[x, y] = random.randint(0, 1)
+                self.grid[0][x, y] = random.randint(0, 1)
 
     def smooth_map(self, factor=1):
-        pass
+        for iteration in range(factor):
+            for x in range(grid_width):
+                for y in range(grid_height):
+                    surrounding_count = len(self.get_surrounding(x, y))
+                    if surrounding_count > 4:
+                        self.grid[0][x, y] = 1
+                    elif surrounding_count < 4:
+                        self.grid[0][x, y] = 0
+                    self.display(False, x, y)
 
     def connect_rooms(self, passage_size=5):
         pass
 
-    def display(self):
-        for x in range(grid_width):
-            for y in range(grid_height):
-                color = self.initial_grid[x, y] * 255
-                pygame.draw.rect(screen, [color, color, color],
-                                 (tile_width * x, tile_height * y, tile_width, tile_height))
-                # pygame.display.update((tile_width * x, tile_height * y, tile_width, tile_height))
+    def display(self, full=True, x=0, y=0):
+        if full:
+            for x in range(grid_width):
+                for y in range(grid_height):
+                    color = self.grid[0][x, y] * 255
+                    pygame.draw.rect(screen, [color, color, color],
+                                     (tile_width * x, tile_height * y, tile_width, tile_height))
+                    # pygame.display.update((tile_width * x, tile_height * y, tile_width, tile_height))
+        else:
+            color = [self.grid[0][x, y] * 255] * 3
+            pygame.draw.rect(screen, color,
+                             (tile_width * x, tile_height * y, tile_width, tile_height))
+            pygame.display.update((tile_width * x, tile_height * y, tile_width, tile_height))
 
     def regenerate(self, seed=None):
         self.seed = seed if seed else hash(str(datetime.datetime.now()))
         self.generate_initial_grid()
         self.display()
+
+    def get_surrounding(self, x, y, distance=1, wall=True, space=False):
+        surrounding_coordinates = [
+            (x, y - distance),
+            (x + distance, y),
+            (x, y + distance),
+            (x - distance, y),
+            (x - distance, y - distance),
+            (x + distance, y + distance),
+            (x - distance, y + distance),
+            (x + distance, y - distance)
+        ]
+
+        return [self.grid[0][surrounding_coordinate] for surrounding_coordinate in surrounding_coordinates
+                if not self.is_out_of_bounds(surrounding_coordinate)
+                and ((wall and self.grid[0][surrounding_coordinate] == 1)
+                     or (space and self.grid[0][surrounding_coordinate] == 0))]
+
+    @staticmethod
+    def is_out_of_bounds(coordinates):
+        x, y = coordinates
+        return not (0 <= x < grid_width and 0 <= y < grid_height)
 
 
 grid_width = 250
@@ -62,4 +96,8 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_F5:
                 main.regenerate()
+            if event.key == pygame.K_s:
+                main.smooth_map()
+            if event.key == pygame.K_c:
+                main.connect_rooms()
     pygame.display.flip()
